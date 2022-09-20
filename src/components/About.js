423d36a29488fbb987177c4c3bac54c6dc88c3c4
Edward <edward.ochieng@student.moringaschool.com>
2022-09-20 08:12:40 +0300

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ function About() {
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src= "https://us.123rf.com/450wm/valiva/valiva1903/valiva190300006/118980125-hand-drawn-rose-bud-on-stem-with-leaves-isolated-on-white-background-pencil-drawing-monochrome-flowe.jpg?ver=6 "alt="I made this" />
     </div>
   );
 }
+export default About;
